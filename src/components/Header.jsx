42a8c64e2e91909ae5b8a88443f93d54b07c7de3
Ashley <amgesmundo@gmail.com>
2019-06-21 19:33:24 +0000

    created class Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,3 +11,27 @@
     // check that your links work!
 
 import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        const {} = this.props;
+        return (
+            <ul className="Header">
+                <li className="Logo">
+                    Catstagram
+                </li>
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Profile
+                </li>
+            </ul>
+            )
+    }
+}
+
+export default Header;
